5aaf8d9f185bdde700af56cf2bb1eeaf20ce8e33
Sebastian Brannstrom <teknolog@gmail.com>
2021-11-04 14:21:09 -0400

    add chillsky

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Groove
 
-A Mac OS status bar app for playing SomaFM's [Groove Salad](https://somafm.com/groovesalad/) stream - which they describe as _A nicely chilled plate of ambient/downtempo beats and grooves._ I like to listen to this while I work.
+A Mac OS status bar app for playing ambient beats while I work. I wanted a minimal UI to turn on and off the stream.
 
-There is already the more complete [SomaFM Radio Player](https://apps.apple.com/us/app/somafm-radio-player/id449155338?mt=12) app, but I wanted a minimal UI to turn on and off this one stream.
+Currently supports these streams
+
+*  [Groove Salad from SomaFM](https://somafm.com/groovesalad/)
+* [Chillsky](https://www.chillsky.com)
 
 ![Screenshot](/screenshot.png)
 
@@ -14,7 +17,7 @@ Then build using
 ```bash
 > python3 -m venv venv
 > source venv/bin/activate
-> pip install -r requirements
+> pip3 install -r requirements.txt
 > make build-prod
 ``` 
 and you'll find `groove.app` in the `dist` directory.
--- groove.py
+++ groove.py
@@ -1,38 +1,61 @@
 import rumps
 import vlc
 
-STREAM_URL = 'https://ice4.somafm.com/groovesalad-128-mp3'
+STREAMS = {
+    'Groove Salad': 'https://ice4.somafm.com/groovesalad-128-mp3',
+    'Chillhop Radio': 'http://hyades.shoutca.st:8043/stream'
+}
+
+DEFAULT_STREAM = 'Groove Salad'
+
+# rumps.debug_mode(True)
 
 
 class GrooveApp(rumps.App):
     def __init__(self, name):
-        super(GrooveApp, self).__init__('Groove App', icon='stop.png', template=True)
-        self.player = vlc.MediaPlayer(STREAM_URL)
+        self.player = None
 
-        self.events = self.player.event_manager()
-        self.events.event_attach(vlc.EventType.MediaPlayerPlaying, self.vlc_event)
-        self.events.event_attach(vlc.EventType.MediaPlayerStopped, self.vlc_event)
+        menu = [rumps.MenuItem(s, callback=self.menu_click) for s in STREAMS]
+        super(GrooveApp, self).__init__('Groove App', menu=menu, icon='stop.png', template=True)
 
-    def vlc_event(self, event):
+    def vlc_event(self, event, stream_name):
         playing = event.type == vlc.EventType.MediaPlayerPlaying
 
         self.icon = 'play.png' if playing else 'stop.png'
-        self.menu['Play'].state = playing
+
+        for m in self.menu:
+            self.menu[m].state = playing and self.menu[m].title == stream_name
 
     # i haven't found a better way to set the initial menu state to -1
     @rumps.timer(0.1)
     def timeout(self, sender):
         sender.stop()
-        self.menu['Play'].state = -1
-        self.player.play()
 
-    @rumps.clicked('Play')
-    def play(self, sender):
+        for m in STREAMS:
+            self.menu[m].state = -1
+
+        self.start_stream(DEFAULT_STREAM)
+
+    def menu_click(self, sender):
+        print(sender.title)
+
         if sender.state == 0:
             sender.state = -1
-            self.player.play()
+            self.start_stream(sender.title)
         elif sender.state == 1:
             self.player.stop()
+
+    def start_stream(self, stream_name):
+        if self.player:
+            self.player.stop()
+
+        self.player = vlc.MediaPlayer(STREAMS[stream_name])
+
+        self.events = self.player.event_manager()
+        self.events.event_attach(vlc.EventType.MediaPlayerPlaying, self.vlc_event, stream_name)
+        self.events.event_attach(vlc.EventType.MediaPlayerStopped, self.vlc_event, stream_name)
+
+        self.player.play()
 
 
 if __name__ == '__main__':
